42f5ea526a752777988e29ec87c755d86a195412
mablds <marcelobragalemos@gmail.com>
2020-09-07 17:07:29 -0300

    Post produtos endpoint cirada

--- src/main/java/holandes/voador/pi4webstorebackend/Controllers/ProdutosController.java
+++ src/main/java/holandes/voador/pi4webstorebackend/Controllers/ProdutosController.java
@@ -11,6 +11,7 @@ package holandes.voador.pi4webstorebackend.Controllers;
  */
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -20,5 +21,10 @@ public class ProdutosController {
     @GetMapping("/produtos")
     public String homeInit() {
         return "Lista de Produtos!!";
+    }
+    
+    @PostMapping("/produtos")
+    public String cadastroProduto() {
+        return "Teste executado com sucesso";
     }
 }
