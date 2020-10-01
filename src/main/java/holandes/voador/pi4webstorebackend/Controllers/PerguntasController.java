87dfdbe012ccd2f703e83918d36f46b3c83fe4b3
Arthur Sakemi <arthursakemi@gmail.com>
2020-10-01 18:00:16 -0300

    rota de adição de perguntas adicionada

--- src/main/java/holandes/voador/pi4webstorebackend/Controllers/PerguntasController.java
+++ src/main/java/holandes/voador/pi4webstorebackend/Controllers/PerguntasController.java
@@ -6,9 +6,12 @@
 package holandes.voador.pi4webstorebackend.Controllers;
 
 import holandes.voador.pi4webstorebackend.DAO.PerguntaDAO;
+import holandes.voador.pi4webstorebackend.Model.Pergunta;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,5 +25,10 @@ public class PerguntasController {
     @DeleteMapping("/pergunta/{id}")
     public static boolean deletePergunta(@PathVariable int id) {
         return PerguntaDAO.deletePergunta(id);
+    }
+
+    @PostMapping("/pergunta")
+    public static boolean addPergunta(@RequestBody Pergunta pergunta) {
+        return PerguntaDAO.addPergunta(pergunta);
     }
 }
--- src/main/java/holandes/voador/pi4webstorebackend/DAO/PerguntaDAO.java
+++ src/main/java/holandes/voador/pi4webstorebackend/DAO/PerguntaDAO.java
@@ -5,10 +5,12 @@
  */
 package holandes.voador.pi4webstorebackend.DAO;
 
+import holandes.voador.pi4webstorebackend.Model.Pergunta;
 import holandes.voador.pi4webstorebackend.utils.GerenciadorConexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -26,6 +28,31 @@ public class PerguntaDAO {
             deleteStatement.setInt(1, id);
             deleteStatement.executeUpdate();
             deleteStatement.close();
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            resposta = false;
+        } finally {
+            GerenciadorConexao.fecharConexao();
+        }
+
+        return resposta;
+    }
+
+    public static boolean addPergunta(Pergunta newPergunta) {
+        Connection conexao;
+        boolean resposta = true;
+
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+            PreparedStatement addStatement = conexao.prepareStatement("INSERT INTO perguntas "
+                    + "(id_produto, pergunta, resposta) "
+                    + "VALUES (?, ?, ?);",
+                    Statement.RETURN_GENERATED_KEYS);
+            addStatement.setInt(1, newPergunta.getIdProduto());
+            addStatement.setString(2, newPergunta.getPergunta());
+            addStatement.setString(3, newPergunta.getResposta());
+            addStatement.executeUpdate();
+
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println(ex.getMessage());
             resposta = false;
