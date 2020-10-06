404a6e3a20a03180706ff1507286da0f84f179f8
Arthur Sakemi <arthursakemi@gmail.com>
2020-10-06 12:08:34 -0300

    pequena refatoração no metodo de busca de cliente por id

--- src/main/java/holandes/voador/pi4webstorebackend/DAO/UsuarioDAO.java
+++ src/main/java/holandes/voador/pi4webstorebackend/DAO/UsuarioDAO.java
@@ -24,14 +24,14 @@ public class UsuarioDAO {
 
     public static ArrayList<Usuario> getAllUsers() {
         Connection conexao = null;
-        PreparedStatement usuarioStatement = null;
+        PreparedStatement statement = null;
         ArrayList<Usuario> usuarios = new ArrayList<>();
 
         try {
             conexao = GerenciadorConexao.abrirConexao();
-            usuarioStatement = conexao.prepareStatement("SELECT * FROM usuarios WHERE ativo = true;");
+            statement = conexao.prepareStatement("SELECT * FROM usuarios WHERE ativo = true;");
 
-            ResultSet rsUsuario = usuarioStatement.executeQuery();
+            ResultSet rsUsuario = statement.executeQuery();
             while (rsUsuario.next()) {
 
                 int id = rsUsuario.getInt("id");
@@ -44,6 +44,7 @@ public class UsuarioDAO {
 
                 usuarios.add(new Usuario(id, nome, cpf, email, senha, cargo, ativo));
             }
+            statement.close();
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
 
@@ -74,6 +75,8 @@ public class UsuarioDAO {
             usuario.setId(idUsuario);
             usuario.setAtivo(true);
 
+            statement.close();
+
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println(ex.getMessage());
             usuario = null;
@@ -83,46 +86,37 @@ public class UsuarioDAO {
         return usuario;
     }
 
-    //Por segurança esse método não retorna a senha
     public static Usuario getUserById(int idUsuario) {
+        Connection conexao;
+        PreparedStatement statement = null;
+        Usuario usuario = null;
 
-        Connection conexao = null;
-        PreparedStatement st = null;
-        Usuario usuario = new Usuario();
         try {
             conexao = GerenciadorConexao.abrirConexao();
-            st = conexao.prepareStatement("SELECT * FROM usuarios WHERE id = ?;");
-            st.setInt(1, idUsuario);
+            statement = conexao.prepareStatement("SELECT * FROM usuarios WHERE id = ?;");
+            statement.setInt(1, idUsuario);
 
-            ResultSet rsUsuario = st.executeQuery();
-            rsUsuario.next();
+            ResultSet rsUsuario = statement.executeQuery();
+            if (rsUsuario.next()) {
+                int id = rsUsuario.getInt("id");
+                String nome = rsUsuario.getString("nome");
+                String cpf = rsUsuario.getString("cpf");
+                String email = rsUsuario.getString("email");
+                String senha = "*****";
+                String cargo = rsUsuario.getString("cargo");
+                boolean ativo = rsUsuario.getBoolean("ativo");
 
-            int id = rsUsuario.getInt("id");
-            String nome = rsUsuario.getString("nome");
-            String cpf = rsUsuario.getString("cpf");
-            String email = rsUsuario.getString("email");
-            //String senha = rsUsuario.getString("senha"); por seguranca não sera retornada a senha
-            String cargo = rsUsuario.getString("cargo");
-            boolean ativo = rsUsuario.getBoolean("ativo");
-
-            usuario.setId(id);
-            usuario.setNome(nome);
-            usuario.setCpf(cpf);
-            usuario.setEmail(email);
-            //usuario.setSenha(senha); por segurança não será retornada a senha
-            usuario.setCargo(cargo);
-            usuario.setAtivo(ativo);
+                usuario = new Usuario(id, nome, cpf, email, senha, cargo, ativo);
+            }
+            statement.close();
 
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+            usuario = null;
         } finally {
-            try {
-                st.close();
-                GerenciadorConexao.fecharConexao();
-            } catch (SQLException ex) {
-            }
+            GerenciadorConexao.fecharConexao();
         }
-        return new Usuario();
+        return usuario;
     }
 
     //TODO
